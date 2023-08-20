0ae957b6999a84ea62d0634fd6774fd7a1e389bb
NEBULA1978 <rpascualey@gmail.com>
2023-08-20 12:08:12 +0200

    Primera parte creando componente botones vue3 cambiando texto de botones añadiento dexto debajo tex area

--- src/components/ButtonCreate.vue
+++ src/components/ButtonCreate.vue
@@ -8,6 +8,8 @@
             </button>
         </div>
         <div :class="{ test: isBoxExpanded }" id="testbox"></div>
+        <textarea v-model="textAreaValue" placeholder="Introduce tu texto aquí"></textarea>
+        <button @click="saveTextArea">Guardar texto</button>
     </div>
 </template>
 
@@ -16,6 +18,7 @@ import { ref } from 'vue';
 
 const numButtons = ref(0);
 const isBoxExpanded = ref(false);
+const textAreaValue = ref('');
 
 const toggleBox = () => {
     isBoxExpanded.value = !isBoxExpanded.value;
@@ -25,12 +28,17 @@ const createButtons = () => {
     const buttonContainer = document.getElementById('buttonContainer');
     buttonContainer.innerHTML = '';
 
-    for (let i = 1; i <= numButtons.value; i++) {  // Start button index from 1
+    for (let i = 1; i <= numButtons.value; i++) {
         const button = document.createElement('button');
-        button.innerText = `Botón ${i}`;  // Set button text
+        button.innerText = `Botón ${i}`;
         button.addEventListener('click', toggleBox);
         buttonContainer.appendChild(button);
     }
+};
+
+const saveTextArea = () => {
+    // Do something with the textAreaValue, e.g., send it to the server or process it
+    console.log('Texto guardado:', textAreaValue.value);
 };
 </script>
 
