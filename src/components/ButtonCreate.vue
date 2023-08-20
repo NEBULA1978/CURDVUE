4b2446d4221aed6876f961a8736e4911c302dc79
NEBULA1978 <rpascualey@gmail.com>
2023-08-20 12:31:27 +0200

    Añado texto dentro de tex area de boton

--- src/components/ButtonCreate.vue
+++ src/components/ButtonCreate.vue
@@ -3,11 +3,11 @@
         <input v-model="numButtons" type="number" placeholder="Número de botones" />
         <button @click="createButtons">Crear botones</button>
         <div id="buttonContainer">
-            <button v-for="buttonIndex in numButtons" :key="buttonIndex" @click="toggleBox">
+            <button v-for="buttonIndex in numButtons" :key="buttonIndex" @click="toggleBox(buttonIndex)">
                 Botón {{ buttonIndex }}
             </button>
         </div>
-        <div :class="{ test: isBoxExpanded }" id="testbox"></div>
+        <div :class="{ test: isBoxExpanded }" id="testbox">{{ savedText }}</div>
         <textarea v-model="textAreaValue" placeholder="Introduce tu texto aquí"></textarea>
         <button @click="saveTextArea">Guardar texto</button>
     </div>
@@ -19,26 +19,21 @@ import { ref } from 'vue';
 const numButtons = ref(0);
 const isBoxExpanded = ref(false);
 const textAreaValue = ref('');
+const buttonTexts = ref([]);
+const savedText = ref('');
 
-const toggleBox = () => {
+const toggleBox = (buttonIndex) => {
     isBoxExpanded.value = !isBoxExpanded.value;
+    console.log(`Botón ${buttonIndex} clicado`);
 };
 
 const createButtons = () => {
-    const buttonContainer = document.getElementById('buttonContainer');
-    buttonContainer.innerHTML = '';
-
-    for (let i = 1; i <= numButtons.value; i++) {
-        const button = document.createElement('button');
-        button.innerText = `Botón ${i}`;
-        button.addEventListener('click', toggleBox);
-        buttonContainer.appendChild(button);
-    }
+    buttonTexts.value = Array.from({ length: numButtons.value }, (_, index) => `Botón ${index + 1}`);
 };
 
 const saveTextArea = () => {
-    // Do something with the textAreaValue, e.g., send it to the server or process it
-    console.log('Texto guardado:', textAreaValue.value);
+    savedText.value = textAreaValue.value;
+    textAreaValue.value = '';
 };
 </script>
 
