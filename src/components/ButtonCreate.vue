ecc5878347d0ce0537e22544b5a0362beed61ca7
NEBULA1978 <rpascualey@gmail.com>
2023-08-20 12:05:10 +0200

    Primera parte creando componente botones vue3 cambiando texto de botones

--- src/components/ButtonCreate.vue
+++ src/components/ButtonCreate.vue
@@ -3,7 +3,9 @@
         <input v-model="numButtons" type="number" placeholder="Número de botones" />
         <button @click="createButtons">Crear botones</button>
         <div id="buttonContainer">
-            <button v-for="buttonIndex in numButtons" :key="buttonIndex" @click="toggleBox">Hola</button>
+            <button v-for="buttonIndex in numButtons" :key="buttonIndex" @click="toggleBox">
+                Botón {{ buttonIndex }}
+            </button>
         </div>
         <div :class="{ test: isBoxExpanded }" id="testbox"></div>
     </div>
@@ -23,9 +25,9 @@ const createButtons = () => {
     const buttonContainer = document.getElementById('buttonContainer');
     buttonContainer.innerHTML = '';
 
-    for (let i = 0; i < numButtons.value; i++) {
+    for (let i = 1; i <= numButtons.value; i++) {  // Start button index from 1
         const button = document.createElement('button');
-        button.innerText = 'Hola';
+        button.innerText = `Botón ${i}`;  // Set button text
         button.addEventListener('click', toggleBox);
         buttonContainer.appendChild(button);
     }
